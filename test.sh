14960ba91be3ac699041e89935f3ff0c9da51aaf
Vinay <Vinay.Misra@kyndryl.com>
2025-06-08 18:04:26 +0200

    test pipeline ci cd

--- test.sh
+++ test.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
 echo "Hi, Testing Github Jenkins integration."
 
 echo "Running test script..."
 echo "Hello from Jenkins!"
 echo "yet another test"
+
+# Use Minikube's Docker daemon
+eval $(minikube docker-env)
+
+# Build the Docker image
+docker build -t yourdockerhubusername/myapp:latest .
